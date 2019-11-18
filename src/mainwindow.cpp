eb70d6eeaafc73b8db3e3ed05a2d0b7b208a6778
Denio <dennis.netterdon@me.com>
2019-11-18 17:25:59 +0100

    change message box when change currency

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -430,8 +430,8 @@ void MainWindow::setupSettingsModal() {
        QObject::connect(settings.comboBoxCurrency, &QComboBox::currentTextChanged, [=] (QString currency_name) {
             this->slot_change_currency(currency_name);
             
-             // Tell the user that it will take some seconds
-            QMessageBox::information(this, tr("Currency Change"), tr("This change will take some seconds"), QMessageBox::Ok);
+             // Tell the user to restart
+            QMessageBox::information(this, tr("Currency Change"), tr("Please restart SilentDragonLite to have new currencies apply"), QMessageBox::Ok);
           
             
              });
